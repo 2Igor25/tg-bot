e664e6f837f1d9bbfc6ebe30cad5cbaa931ad52e
Igor Terekhin <igor.terekhin.02@mail.ru>
2023-10-16 21:28:43 +0500

    initial commit

---
+++ src/main/java/ru/igorter/tgbot/TgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.igorter.tgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotApplication.class, args);
+	}
+
+}
